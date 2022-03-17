a7a78a8962aea5d2cdf9cd744ecc515a580e2021
Michel Metran <michelmetran@gmail.com>
2022-03-17 17:35:48 -0300

    Ajustes

--- test/paths.py
+++ test/paths.py
@@ -2,16 +2,17 @@ import os
 
 
 # Set Directory
-tabs_path = os.path.join('..', 'data', 'tabs')
-shps_path = os.path.join('..', 'data', 'shps')
-gpkg_path = os.path.join('..', 'data', 'gpkg')
-zips_path = os.path.join('..', 'data', 'zips')
+data_path = os.path.join('..', 'data')
+tabs_path = os.path.join(data_path, 'tabs')
+shps_path = os.path.join(data_path, 'shps')
+gpkg_path = os.path.join(data_path, 'gpkg')
+zips_path = os.path.join(data_path, 'zips')
+gjson_path = os.path.join(data_path, 'gjson')
+
 
 # Create Directory
 os.makedirs(shps_path, exist_ok=True)
 os.makedirs(tabs_path, exist_ok=True)
 os.makedirs(gpkg_path, exist_ok=True)
 os.makedirs(zips_path, exist_ok=True)
-
-
-
+os.makedirs(gjson_path, exist_ok=True)
